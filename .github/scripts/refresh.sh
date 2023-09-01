8ef470bb5eb845e58555aa2cc639f6f0f6e03fb5
Philipp Gayret <philipp@hileco.com>
2023-09-01 16:21:29 +0200

    fix: Install bsdtar during refresh

--- .github/scripts/refresh.sh
+++ .github/scripts/refresh.sh
@@ -9,7 +9,7 @@ WORK_DIR=$(mktemp -d)
 trap 'rm -rf "$WORK_DIR"' EXIT
 
 for DOWNLOAD_URL in $(echo "$ARTIFACTS" | jq -r ".archive_download_url" | head -n "$ARTIFACTS_LIMIT"); do
-    gh api "$DOWNLOAD_URL" | bsdtar -xvf - -C "$WORK_DIR"
+    gh api "$DOWNLOAD_URL" | bsdtar -xvf - -C "$WORK_DIR" || continue
     EVENT_NAME=$(jq -r ".event_name" < "$WORK_DIR/context_github.json")
     EVENT_ACTION=$(jq -r ".event.action" < "$WORK_DIR/context_github.json")
     EVENT_PATH="on/$EVENT_NAME"
